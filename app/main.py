90eaae9d3ed0534f6a6193a2ba71687f02dfc27e
YiFangLu <technology99x@gmail.com>
2023-12-20 21:00:58 +0800

    Add CORS Add restaurant api Add gunicorn

--- app/main.py
+++ app/main.py
@@ -1,10 +1,30 @@
+import logging
 import os
-from typing import Union
 
+import googlemaps
+import toml
 import uvicorn
-from fastapi import FastAPI
+from fastapi import FastAPI, Depends, HTTPException
+from fastapi.middleware.cors import CORSMiddleware
+
+from inOutSchema import LatLngIn
+
+config = toml.load('config.toml')
 
 app = FastAPI()
+
+origins = config['origins']
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=False,
+    allow_methods=["GET"],
+    allow_headers=["Content-Type"],
+)
+
+GOOGLE_MAP_KEY = config['google']['map_key']
+gmaps = googlemaps.Client(key=GOOGLE_MAP_KEY)
 
 
 @app.get("/")
@@ -12,9 +32,20 @@ def read_root():
     return {"Hello": "World"}
 
 
-@app.get("/items/{item_id}")
-def read_item(item_id: int, q: Union[str, None] = None):
-    return {"item_id": item_id, "q": q}
+@app.get("/restaurants/{lat}/{lng}")
+def find_restaurants(params: LatLngIn = Depends()):
+    try:
+        restaurants = gmaps.places_nearby(
+            {'lat': params.lat, 'lng': params.lng},
+            type="restaurant", radius=5000, language="zh-TW", open_now=True
+        )['results']
+    except Exception as e:
+        msg = 'Google map api error'
+        logging.info(f'{msg}: {e}')
+        raise HTTPException(status_code=500, detail=msg)
+
+    return [restaurant['name'] for restaurant in restaurants]
+
 
 if __name__ == "__main__":
     uvicorn.run(app, host="0.0.0.0", port=int(os.environ.get("PORT", 8080)))
